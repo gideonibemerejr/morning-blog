b5fd9accb5f2f152724a80adf02d24bc8803d75e
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-07-07 02:16:40 -0500

    Update button

--- src/Button/index.js
+++ src/Button/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-function Button({ handleClick, type }) {
-  return <button onClick={handleClick}>{type}</button>
+function Button({ handleClick, handleDeletePost, index, type }) {
+  if (handleClick) {
+    return <button onClick={handleClick}>{type}</button>
+  } else {
+    return <button onClick={() => handleDeletePost(index)}>{type}</button>
+  }
 }
+
 export default Button
